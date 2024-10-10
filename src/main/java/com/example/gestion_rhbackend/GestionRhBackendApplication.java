5cd797cf94d0fb04132870d24c0fe02654a08521
fatima_ezzahra <fatimaezzahrasafrani@gmail.com>
2024-10-10 08:27:33 +0000

    First commit

---
+++ src/main/java/com/example/gestion_rhbackend/GestionRhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_rhbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRhBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionRhBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestion_rhbackend/GestionRhBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_rhbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRhBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
