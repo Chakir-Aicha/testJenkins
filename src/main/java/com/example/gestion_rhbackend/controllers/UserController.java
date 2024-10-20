94e36de0089eff4416bacd6088bdecf25a9ccab8
fatima_ezzahra <fatimaezzahrasafrani@gmail.com>
2024-10-20 19:41:57 +0000

    fusion

--- src/main/java/com/example/gestion_rhbackend/controllers/UserController.java
+++ src/main/java/com/example/gestion_rhbackend/controllers/UserController.java
@@ -4,12 +4,14 @@ import com.example.gestion_rhbackend.dtos.UserDto;
 import com.example.gestion_rhbackend.services.UserManagementServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
     private UserManagementServiceImpl usersManagementService;
 
